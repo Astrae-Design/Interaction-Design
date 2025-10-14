4e3af0d509fbb28f62478d90fdc42eb75570c267
Braimah-Abiola <braimahabiola.official@gmail.com>
2025-10-14 14:46:02 +0000

    Updates

--- faq-sections/src/app/(routes)/faq-2/page.tsx
+++ faq-sections/src/app/(routes)/faq-2/page.tsx
@@ -13,7 +13,7 @@ import { motion } from "framer-motion";
 const Faq = () => {
 
     return (
-        <section className=" w-full min-h-screen flex items-center justify-center bg-white">
+        <section className=" w-full min-h-screen flex items-center justify-center bg-white py-40">
             <div className=" w-full p-1.5 bg-secondary border rounded-xl max-w-2xl mx-auto">
                 <Accordion type="single" className=" flex flex-col gap-1.5" collapsible>
                     <motion.div
