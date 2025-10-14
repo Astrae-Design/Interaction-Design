2fa6a4fbc327681fdde640ea752f9b0e169da83e
Braimah-Abiola <braimahabiola.official@gmail.com>
2025-10-14 14:18:20 +0000

    Add two new component batches

---
+++ animated-buttons/src/app/(routes)/ring-button/page.tsx
@@ -0,0 +1,11 @@
+import RingButton from "@/app/components/buttons/ring-button";
+
+const HomePage = () => {
+    return (
+        <div className="w-screen h-screen bg-white items-center justify-center flex">
+            <RingButton text="Astrae's Amazing" />
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ faq-sections/src/app/(routes)/faq-2/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { containerVariants, itemVariants } from "@/anim";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion-two";
+import { faqData } from "@/constants";
+import { motion } from "framer-motion";
+
+const Faq = () => {
+
+    return (
+        <section className=" w-full min-h-screen flex items-center justify-center bg-white">
+            <div className=" w-full p-1.5 bg-secondary border rounded-xl max-w-2xl mx-auto">
+                <Accordion type="single" className=" flex flex-col gap-1.5" collapsible>
+                    <motion.div
+                        variants={containerVariants}
+                        initial="hidden"
+                        animate="visible"
+                        className="flex flex-col gap-1.5"
+                    >
+                        {faqData.map((faq, index) => (
+                            <motion.div key={index} variants={itemVariants}>
+                                <AccordionItem value={faq.id}>
+                                    <AccordionTrigger>{faq.question}</AccordionTrigger>
+                                    <AccordionContent>
+                                        {faq.answer}
+                                    </AccordionContent>
+                                </AccordionItem>
+                            </motion.div>
+                        ))}
+                    </motion.div>
+                </Accordion>
+            </div>
+        </section>
+    );
+}
+
+export default Faq;
