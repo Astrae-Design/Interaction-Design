c9a12e284677a51f17a65c3534dd8418527260ea
Braimah-Abiola <braimahabiola.official@gmail.com>
2025-12-11 19:13:28 +0000

    Add glow button

---
+++ animated-buttons/src/app/(routes)/glow-button/page.tsx
@@ -0,0 +1,11 @@
+import GlowButton from "../../components/buttons/glow-button";
+
+const GlowButtonPage = () => {
+    return (
+        <div className="w-screen h-screen bg-black items-center justify-center flex">
+            <GlowButton text="Glow in the dark" />
+        </div>
+    );
+}
+
+export default GlowButtonPage;
--- logo-clouds/src/sections/company-logos.tsx
+++ logo-clouds/src/sections/company-logos.tsx
@@ -11,11 +11,11 @@ const CompanyLogos = () => {
         <section className=" w-full py-8 z-20 relative flex flex-col items-center mt-28">
             <h6 className=" font-medium opacity-80 text-base md:text-lg">Trusted by 24+ Startups and Businesses for Design and Development</h6>
             <div className=" max-w-6xl mx-auto w-full mt-8">
-                <div className="grid grid-cols-6 w-full relative group">
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 w-full relative group">
                     {companyLogos.map((logo, index) => (
-                        <div key={index} className={`w-full border bg-[#F6F6F6] py-4 flex items-center justify-center ${index === 0 ? 'rounded-tl-xl' :
-                            index === 5 ? 'rounded-tr-xl' :
-                                index === 18 ? 'rounded-bl-xl' : ''
+                        <div key={index} className={`w-full border bg-[#F6F6F6] py-4 flex items-center justify-center ${index === 0 ? 'xl:rounded-tl-xl' :
+                            index === 5 ? 'xl:rounded-tr-xl' :
+                                index === 18 ? 'xl:rounded-bl-xl' : ''
                             }`}>
                             <motion.div
                                 className="relative flex items-center justify-center object-contain"
@@ -43,8 +43,8 @@ const CompanyLogos = () => {
                             </motion.div>
                         </div>
                     ))}
-                    <div className=" w-full border py-2.5 px-2.5 rounded-br-xl bg-[#F6F6F6]">
-                        <div className=" text-sm md:text-[15px] bg-[#F6F6F6] border border-dashed hover:border-none hover:bg-[#002BBA] hover:text-white cursor-pointer border-[#002BBA] w-full h-full text-[#002BBA] font-medium gap-1 rounded-lg flex items-center justify-center">
+                    <div className=" w-full border py-2.5 px-2.5 xl:rounded-br-xl bg-[#F6F6F6]">
+                        <div className=" text-sm md:text-[15px] bg-[#F6F6F6] border border-dashed hover:border-none hover:bg-[#002BBA] hover:text-white cursor-pointer border-[#002BBA] w-full h-full text-[#002BBA] font-medium gap-1 xl:rounded-lg flex items-center justify-center">
                             <p>Your Logo</p> <Plus className=" size-4" />
                         </div>
                     </div>
